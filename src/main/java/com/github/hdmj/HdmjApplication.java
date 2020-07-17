ccda5682d52db4e3d0c2366eef8c805d039cf341
王帅 <wangsss183@gmail.com>
2020-07-17 11:52:38 +0800

    项目初始化

---
+++ src/main/java/com/github/hdmj/HdmjApplication.java
@@ -0,0 +1,13 @@
+package com.github.hdmj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdmjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdmjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/hdmj/HdmjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hdmj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdmjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
